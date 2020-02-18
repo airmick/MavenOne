a73c1dce02129c2a5425ac4f6e2a4fc8f82ccc30
Josmi <aelmick92@gmail.com>
2020-02-18 23:32:38 +0200

    first merge with branch joel

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,34 +5,44 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.IntStream;
 
 /**
- * Hello world!
+ * Random Number
  */
 public class App {
     public static void main(String[] args) {
 
         int[] randomNumbers = IntStream.generate(
-                                () -> ThreadLocalRandom.current().nextInt(0, 10000 + 1))
-                                        .limit(100)
-                                        .boxed()
-                                        .mapToInt(i -> i)
-                                        .toArray();
-
-        int largestNumber = Arrays.stream(randomNumbers)
-                                    .max()
-                                    .getAsInt();
-
-        int smallestNumber = Arrays.stream(randomNumbers)
-                                    .min()
-                                    .getAsInt();
+                () -> ThreadLocalRandom.current().nextInt(0, 10000 + 1))
+                .limit(100)
+                .boxed()
+                .mapToInt(i -> i)
+                .toArray();
 
         System.out.println("Printing the whole array...");
         Arrays.stream(randomNumbers)
                 .forEach(System.out::println);
+        System.out.println("Sorted Array ");
+        sortingArray(randomNumbers);
+        System.out.print(Arrays.toString(randomNumbers));
 
-        System.out.println("\n\nThe largest number is: " + largestNumber);
-
-
-
-
+        System.out.println("\n\nThe largest number is: " + findLargestNumber(randomNumbers));
+        System.out.println("\n\nThe smallest number is: " + findSmallestNumber(randomNumbers));
+    }
+    private static int findLargestNumber(int[] getRandomNumber)
+    {
+        int largestNumber = Arrays.stream(getRandomNumber)
+                .max()
+                .getAsInt();
+        return largestNumber;
+    }
+    private static int findSmallestNumber(int[] n)
+    {
+        int smallestNumber = Arrays.stream(n)
+                .min()
+                .getAsInt();
+        return smallestNumber;
+    }
+    private static void sortingArray(int[] n)
+    {
+        Arrays.sort(n);
     }
 }
